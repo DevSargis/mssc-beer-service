e33c3d32fe6842896c147b0e8c5f7893f2f5562e
Sargis Margaryan <sargis.margaryan98@mail.ru>
2023-06-08 05:38:36 -0700

    third commit

--- src/main/java/guru/springframwork/msscbeerservice/Model/BeerDto.java
+++ src/main/java/guru/springframwork/msscbeerservice/Model/BeerDto.java
@@ -1,9 +1,13 @@
 package guru.springframwork.msscbeerservice.Model;
 
+import guru.springframwork.msscbeerservice.Enums.BeerStyleEnum;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Data
@@ -13,6 +17,11 @@ import java.util.UUID;
 public class BeerDto {
     private UUID id;
     private String beerName;
-    private String beerStyle;
     private String upc;
+    private Integer version;
+    private OffsetDateTime createdData;
+    private OffsetDateTime updatedData;
+    private BigDecimal price;
+    private Integer quantityOnHand;
+    private BeerStyleEnum beerStyle;
 }
